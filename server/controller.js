0074abeda2c9afda1de7a775f92f140dd1338110
the-parthenon <ericinportland@gmail.com>
2022-04-23 05:43:03 -0700

    Initial Database Seed Implemented.

--- server/controller.js
+++ server/controller.js
@@ -11,3 +11,58 @@ const sequelize = new Sequelize(CONNECTION_STRING, {
     },
   },
 });
+
+module.exports = {
+  seed: (req, res) => {
+    sequelize
+      .query(
+        `
+                DROP TABLE IF EXISTS pairs;
+                DROP TABLE IF EXISTS students;
+                DROP TABLE IF EXISTS squads;
+
+                CREATE TABLE squads(
+                    squad_id SERIAL PRIMARY KEY,
+                    squad_name VARCHAR(40)
+                  );
+                
+                CREATE TABLE students(
+                    student_id SERIAL PRIMARY KEY,
+                    first_name VARCHAR(40),
+                    last_name VARCHAR(40),
+                    squad_id INT REFERENCES squads(squad_id)
+                  );
+                  
+                CREATE TABLE pairs(
+                    pair_id SERIAL PRIMARY KEY,
+                    student_one INT REFERENCES students(student_id),
+                    student_two INT REFERENCES students(student_id)
+                  );
+                  
+                  INSERT INTO squads(squad_name)
+                  VALUES('A Squad'),
+                  ('B Squad');  
+                  
+                  INSERT INTO students(first_name, last_name, squad_id)
+                  VALUES ('Amy', 'Adams', 1),
+                  ('Billy', 'Boyd', 1),
+                  ('Chester', 'Cheetah', 1),
+                  ('Davey', 'Dillups', 1),
+                  ('Edmund', 'Everly', 1),
+                  ('Frankie', 'Fivetimes', 1),
+                  ('Gilbert', 'Grape', 2),
+                  ('Harry', 'Hambone', 2),
+                  ('India', 'Illmatic', 2),
+                  ('Julie', 'July', 2),
+                  ('Kelly', 'Kapowski', 2),
+                  ('Leslie', 'Lamour', 2);
+
+            `
+      )
+      .then(() => {
+        console.log('DB seeded!');
+        res.sendStatus(200);
+      });
+    //   .catch((err) => console.log('error seeding DB'), err);
+  },
+};
---
+++ server/index.js
@@ -0,0 +1,14 @@
+require('dotenv').config();
+const express = require('express');
+const app = express();
+const cors = require('cors');
+const { SERVER_PORT } = process.env;
+const { seed } = require('./controller.js');
+
+app.use(express.json());
+app.use(cors());
+
+// DEV
+app.post('/seed', seed);
+
+app.listen(SERVER_PORT, () => console.log(`up on ${SERVER_PORT}`));
