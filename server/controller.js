187d7504deeae080f6b771f7a562b6a222969787
the-parthenon <ericinportland@gmail.com>
2022-05-04 21:22:41 -0700

    Pairing works, except sometimes infinitely loops.

--- server/controller.js
+++ server/controller.js
@@ -88,22 +88,19 @@ module.exports = {
       .then(() => {
         (async function loop() {
           do {
+            console.log(`Everybody else:`, JSON.stringify(everybody, null, 2));
+            //get the past groups for the current student
             let groups = await getPastGroups(everybody[0].id);
-            // console.log(JSON.stringify(groups, null, 2));
-            groups.forEach((element) => {
-              groupArr.push(element.groupId);
-            });
-            // console.log(groupArr);
+            groupArr = groups.map((a) => a.groupId);
+            // use past groups to get all previous pairs
             let pairs = await getPastPairs(groupArr);
-            // console.log(JSON.stringify(pairs, null, 2));
+            // console.log('pairs array: ', JSON.stringify(pairs, null, 2));
             pairs.forEach((element) => {
               pairArr.push(element.studentId);
             });
             for (let i = 1; i < everybody.length; i++) {
               if (!pairArr.includes(everybody[i].id)) {
-                pairedArr.push(
-                  `1. ${everybody[0].firstName} ${everybody[0].lastName} 2. ${everybody[i].firstName} ${everybody[i].lastName}`
-                );
+                pairedArr.push(everybody[0], everybody[i]);
                 everybody.splice(i, 1);
                 everybody.splice(0, 1);
                 groupArr = [];
@@ -111,7 +108,7 @@ module.exports = {
                 break;
               }
             }
-            console.log(pairedArr);
+            // console.log(pairedArr);
           } while (everybody.length > 0);
         })()
           .then(() => {
@@ -130,7 +127,6 @@ module.exports = {
     // let priorPairs = [];
 
     getEverybody().then((studentArr) => {
-      shuffle(studentArr);
       // console.log(JSON.stringify(studentArr, null, 2));
       for (let i = 0; i < studentArr.length; i += 2) {
         pairedArr.push(
@@ -154,12 +150,12 @@ module.exports = {
         { firstName: 'Davey', lastName: 'Dillups' },
         { firstName: 'Edmund', lastName: 'Everly' },
         { firstName: 'Frankie', lastName: 'Fivetimes' },
-        { firstName: 'Gilbert', lastName: 'Grape' },
-        { firstName: 'Harry', lastName: 'Hambone' },
-        { firstName: 'India', lastName: 'Illmatic' },
-        { firstName: 'Julie', lastName: 'July' },
-        { firstName: 'Kelly', lastName: 'Kapowski' },
-        { firstName: 'Leslie', lastName: 'Lamour' },
+        // { firstName: 'Gilbert', lastName: 'Grape' },
+        // { firstName: 'Harry', lastName: 'Hambone' },
+        // { firstName: 'India', lastName: 'Illmatic' },
+        // { firstName: 'Julie', lastName: 'July' },
+        // { firstName: 'Kelly', lastName: 'Kapowski' },
+        // { firstName: 'Leslie', lastName: 'Lamour' },
       ]);
 
       await Group.bulkCreate([
@@ -183,24 +179,24 @@ module.exports = {
         { groupId: 2, studentId: 4 },
         { groupId: 3, studentId: 5 },
         { groupId: 3, studentId: 6 },
-        { groupId: 4, studentId: 7 },
-        { groupId: 4, studentId: 8 },
-        { groupId: 5, studentId: 9 },
-        { groupId: 5, studentId: 10 },
-        { groupId: 6, studentId: 11 },
-        { groupId: 6, studentId: 12 },
-        { groupId: 7, studentId: 2 },
-        { groupId: 7, studentId: 3 },
-        { groupId: 8, studentId: 4 },
-        { groupId: 8, studentId: 5 },
-        { groupId: 9, studentId: 6 },
-        { groupId: 9, studentId: 7 },
-        { groupId: 10, studentId: 8 },
-        { groupId: 10, studentId: 9 },
-        { groupId: 11, studentId: 10 },
-        { groupId: 11, studentId: 11 },
-        { groupId: 12, studentId: 12 },
-        { groupId: 12, studentId: 1 },
+        // { groupId: 4, studentId: 7 },
+        // { groupId: 4, studentId: 8 },
+        // { groupId: 5, studentId: 9 },
+        // { groupId: 5, studentId: 10 },
+        // { groupId: 6, studentId: 11 },
+        // { groupId: 6, studentId: 12 },
+        // { groupId: 7, studentId: 2 },
+        // { groupId: 7, studentId: 3 },
+        // { groupId: 8, studentId: 4 },
+        // { groupId: 8, studentId: 5 },
+        // { groupId: 9, studentId: 6 },
+        // { groupId: 9, studentId: 7 },
+        // { groupId: 10, studentId: 8 },
+        // { groupId: 10, studentId: 9 },
+        // { groupId: 11, studentId: 10 },
+        // { groupId: 11, studentId: 11 },
+        // { groupId: 12, studentId: 12 },
+        // { groupId: 12, studentId: 1 },
       ]);
     })().then(() => {
       console.log('DB seeded!');
