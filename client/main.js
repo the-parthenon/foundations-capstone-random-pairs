6510d67e00bab9de59683c354a0d421b6a629b3b
the-parthenon <ericinportland@gmail.com>
2022-05-10 12:28:05 -0700

    Added additional styling.

--- client/main.js
+++ client/main.js
@@ -5,7 +5,9 @@ const addButton = document.getElementById('add-button');
 const addStudent = document.getElementById('add-student');
 const stuHist = document.getElementById('get-student-history');
 const stuHistDisplay = document.getElementById('display-student-history');
+const stuHistButton = document.getElementById('st-hist-button');
 const pairSpinner = document.getElementById('pair-loading');
+const histSpinner = document.getElementById('hist-loading');
 
 const getPairings = () => {
   console.log(`Going to get pairings!`);
@@ -56,6 +58,8 @@ const getStudents = () => {
 
 const addNewStudent = (evt) => {
   evt.preventDefault();
+  addButton.setAttribute('disabled', '');
+  document.getElementById('add-alert').classList.remove('collapse');
   console.log(`Let's add a student!`);
   let studentName = document.getElementById('student-name');
   console.log(`${studentName.value}`);
@@ -63,24 +67,35 @@ const addNewStudent = (evt) => {
     newName: studentName.value,
   };
   axios.post(`http://localhost:6060/studentadd`, body).then(() => {
+    studentName.value = '';
+    document.getElementById('add-alert').classList.add('collapse');
+    document.getElementById('add-success').classList.remove('collapse');
+    setTimeout(() => {
+      document.getElementById('add-success').classList.add('collapse');
+    }, 3000);
+
     console.log('Student added!');
   });
 };
 
 const getStudentHistory = (evt) => {
   evt.preventDefault();
-  console.log('Fetching History!');
+  stuHistButton.setAttribute('disabled', '');
+  while (stuHistDisplay.firstChild) {
+    stuHistDisplay.removeChild(stuHistDisplay.firstChild);
+  }
+  histSpinner.classList.remove('invisible');
+
+  // console.log('Fetching History!');
   let studentName = document.getElementById('historyList');
   // console.log(studentName);
-  console.log(`${studentName.value}`);
+  // console.log(`${studentName.value}`);
   let body = {
     requestedName: studentName.value,
   };
   axios.post(`http://localhost:6060/studenthist`, body).then((res) => {
-    console.log(res.data);
-    while (stuHistDisplay.firstChild) {
-      stuHistDisplay.removeChild(stuHistDisplay.firstChild);
-    }
+    // console.log(res.data);
+
     let sectionTitle = `<h5>${studentName.value} Has Previously Paired With</h5>`;
     stuHistDisplay.innerHTML += `${sectionTitle}`;
     res.data.forEach((elem) => {
@@ -91,6 +106,8 @@ const getStudentHistory = (evt) => {
 
       histDiv.innerHTML += `${pText}`;
     });
+    stuHistButton.removeAttribute('disabled');
+    histSpinner.classList.add('invisible');
     studentName.value = '';
   });
 };
